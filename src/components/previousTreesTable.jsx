2c44002998f463b7e5ecd55d17fb60781ba52cc5
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-08-15 11:02:19 -0230

    Added code for previousTreesTable.jsx

---
+++ src/components/previousTreesTable.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../App.css';
+
+function PreviousTreesTable() {
+    const [tree, setTree] = useState([])
+
+    useEffect(() => {
+        axios
+        .get('http://localhost:8080/previous-trees')
+        .then(response => {
+            setTree(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching tree data', error)
+        });
+    }, []);
+    return(
+        <div>
+            <ul className = 'jsonTable'>
+                {flight.map(item => (
+                    <li key = {item.id}>
+                        <h3 className = 'jsonTableHead'>Tree {item.id}</h3>
+
+                        <div className = 'jsonTableBody'>
+                            <h3>Airline: {item.treeJson}</h3>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default PreviousTreesTable
